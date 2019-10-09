08803f59578c8d04f34cbdd242eeb9b5c0830c78
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2019-10-08 21:57:06 -0700

    chore: add quic.js
    Fails with the following error:
    TypeError [ERR_INVALID_ARG_TYPE]: The "options" argument must be of type Object. Received type number
        at Object.createSocket (internal/quic/core.js:2313:11)
        at Object.<anonymous> (/home/trivikr/workspace/node-http-servers/quic.js:9:21)
        at Module._compile (internal/modules/cjs/loader.js:946:30)
        at Object.Module._extensions..js (internal/modules/cjs/loader.js:963:10)
        at Module.load (internal/modules/cjs/loader.js:797:32)
        at Function.Module._load (internal/modules/cjs/loader.js:710:12)
        at Function.Module.runMain (internal/modules/cjs/loader.js:1015:10)

---
+++ quic.js
@@ -0,0 +1,24 @@
+const quic = require("quic");
+const fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("ssl/localhost.key"),
+  cert: fs.readFileSync("ssl/localhost.cert")
+};
+
+const socket = quic.createSocket(3000);
+socket.listen(options);
+
+socket.on("session", (session) => {
+  // A new server side session has been created
+  
+  session.on("stream", (stream, headers) => {
+    if (headers[':path'] === "/") {
+      stream.respondWithFile("./files/index.html");
+    } else if (headers[':path'] === "/color.css") {
+      stream.respondWithFile("./files/color.css");
+    } else if (headers[':path'] === "/decor.css") {
+      stream.respondWithFile("./files/decor.css");
+    }
+  });
+});
