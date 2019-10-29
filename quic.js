20aca760fddc7d269a812ac3d2aafc9a8ce43f8c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2019-10-28 21:12:49 -0700

    chore: experimenting with curl

--- quic.js
+++ quic.js
@@ -13,6 +13,12 @@ server.on("session", session => {
   // A new server side session has been created
 
   session.on("stream", (stream, headers) => {
+    // curl fails with following error
+    // (56) nghttp3_conn_read_stream returned error: ERR_HTTP_FRAME_ERROR
+    fs.createReadStream("./files/index.html").pipe(stream);
+    stream.setEncoding("utf8");
+
+    /* Uncomment after headers are added for stream
     if (headers[":path"] === "/") {
       stream.respondWithFile("./files/index.html");
     } else if (headers[":path"] === "/style.css") {
@@ -21,6 +27,6 @@ server.on("session", session => {
       stream.respondWithFile("./files/script.js");
     } else if (headers[":path"] === "/globe.png") {
       stream.respondWithFile("./files/globe.png");
-    }
+    }*/
   });
 });
