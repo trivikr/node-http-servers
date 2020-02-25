7e374e0ec5a97c8970a6fa2b36cfb925d23dacb8
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-25 11:14:43 +0530

    chore: import createServer from http

--- http.js
+++ http.js
@@ -1,4 +1,4 @@
-const https = require("https");
+const { createServer } = require("https");
 const fs = require("fs");
 
 const options = {
@@ -6,15 +6,13 @@ const options = {
   cert: fs.readFileSync("ssl/localhost.cert")
 };
 
-https
-  .createServer(options, (req, res) => {
-    if (req.url === "/") {
-      fs.createReadStream(`./files/index.html`).pipe(res);
-    } else {
-      // regular expression for filename requested
-      const re = /\/(\w+)*/;
-      const filename = req.url.replace(re, "$1");
-      fs.createReadStream(`./files/${filename}`).pipe(res);
-    }
-  })
-  .listen(3000);
+createServer(options, (req, res) => {
+  if (req.url === "/") {
+    fs.createReadStream(`./files/index.html`).pipe(res);
+  } else {
+    // regular expression for filename requested
+    const re = /\/(\w+)*/;
+    const filename = req.url.replace(re, "$1");
+    fs.createReadStream(`./files/${filename}`).pipe(res);
+  }
+}).listen(3000);
