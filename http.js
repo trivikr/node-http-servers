27e85985da0d0088d7f0597e4498d82bdb3d1ee8
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-02-25 11:29:59 +0530

    fix: use pipeline to avoid memory leak (#7)

--- http.js
+++ http.js
@@ -1,18 +1,23 @@
 const { createServer } = require("https");
 const { readFileSync, createReadStream } = require("fs");
+const { pipeline } = require("stream");
 
 const options = {
   key: readFileSync("ssl/localhost.key"),
   cert: readFileSync("ssl/localhost.cert")
 };
 
+const errCallback = err => {
+  if (err) console.log(err);
+};
+
 createServer(options, (req, res) => {
   if (req.url === "/") {
-    createReadStream(`./files/index.html`).pipe(res);
+    pipeline(createReadStream(`./files/index.html`), res, errCallback);
   } else {
     // regular expression for filename requested
     const re = /\/(\w+)*/;
     const filename = req.url.replace(re, "$1");
-    createReadStream(`./files/${filename}`).pipe(res);
+    pipeline(createReadStream(`./files/${filename}`), res, errCallback);
   }
 }).listen(3000);
